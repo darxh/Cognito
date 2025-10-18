418435604dd4f85eeaff6759420980d215961c53
darxh <darshanyavagal@gmail.com>
2025-10-18 19:51:08 +0530

    implementing model

---
+++ Backend/models/Thread.js
@@ -0,0 +1,42 @@
+import mongoose from "mongoose";
+
+const MessageSchema = new mongoose.Schema({
+  role: {
+    type: String,
+    enum: ["user", "model"],
+    required: true,
+  },
+  content: {
+    type: String,
+    required: true,
+  },
+  timestamp: {
+    type: Date,
+    default: Date.now
+  },
+});
+
+const ThreadSchema = new mongoose.Schema({
+  threadId: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  title: {
+    type: String,
+    default: "new-chat",
+  },
+  messages: [MessageSchema],
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Thread = mongoose.model("Thread", ThreadSchema);
+
+export default Thread;
