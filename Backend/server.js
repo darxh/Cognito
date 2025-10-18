878d078d3ebae5000ad10556061b163f963a6cba
darxh <darshanyavagal@gmail.com>
2025-10-18 18:49:32 +0530

    testing model with end-points

--- Backend/server.js
+++ Backend/server.js
@@ -1,14 +1,61 @@
 import "dotenv/config";
 import { GoogleGenAI } from "@google/genai";
 
-const ai = new GoogleGenAI(process.env.GEMINI_API_KEY);
+// const ai = new GoogleGenAI(process.env.GEMINI_API_KEY);
 
-async function main() {
-  const response = await ai.models.generateContent({
-    model: "gemini-2.5-flash",
-    contents: "Explain how AI works in a few words",
-  });
-  console.log(response.text);
-}
+// async function main() {
+//   const response = await ai.models.generateContent({
+//     model: "gemini-2.5-flash",
+//     contents: "Tell me dad joke",
+//   });
+//   console.log(response.text);
+// }
 
-main();
+// main();
+import express from "express";
+import cors from "cors";
+
+const app = express();
+const PORT = 8080;
+
+app.use(express.json());
+app.use(cors());
+
+app.listen(PORT, () => {
+  console.log(`server is running on : ${PORT}`);
+});
+
+app.post("/chat", async (req, res) => {
+  const apiKey = process.env.GEMINI_API_KEY;
+  const URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=${apiKey}`;
+
+  const options = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      contents: [
+        {
+          parts: [
+            {
+              text: req.body.message,
+            },
+          ],
+        },
+      ],
+    }),
+  };
+
+  try {
+    const apiresult = await fetch(URL, options);
+    const data = await apiresult.json();
+
+    const text = data.candidates[0].content.parts[0].text;
+    console.log(data);
+    res.send(text);
+  } catch (err) {
+    console.log(`error while processing the data`, err);
+    res.status(500).send({ error: "failed to fetch data " });
+  }
+});
