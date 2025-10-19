10c3d20dbe500128ca6091e6dcc4e56ea11b46d2
darxh <darshanyavagal@gmail.com>
2025-10-19 12:45:47 +0530

    setup routes

--- Backend/routes/chat.js
+++ Backend/routes/chat.js
@@ -2,13 +2,12 @@ import express from "express";
 const router = express.Router();
 import Thread from "../models/Thread.js";
 
-
 //test
 router.post("/test", async (req, res) => {
   try {
     const thread = new Thread({
-      threadId: "91120",
-      title: "Testing new Thread-3",
+      threadId: "914",
+      title: "Testing new Thread-4",
     });
 
     const response = await thread.save();
@@ -16,6 +15,55 @@ router.post("/test", async (req, res) => {
   } catch (err) {
     console.log("faild to connect the test route", err);
     res.status(500).json({ error: "failed to save in DB" });
+  }
+});
+
+//get all the threads
+router.get("/thread", async (req, res) => {
+  try {
+    const threads = await Thread.find({}).sort({ updatedAt: -1 });
+    console.log(threads);
+    res.json(threads);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: "failed to fetch the all the threads" });
+  }
+});
+
+//to get specific thread
+router.get("/thread/:threadId", async (req, res) => {
+  const { threadId } = req.params;
+  try {
+    const thread = await Thread.findOne({ threadId });
+
+    if (!thread) {
+      res.status(404).json({ error: "Thread not found" });
+    }
+
+    console.log(thread);
+    res.json(thread.messages);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: "failed to fetch this thread" });
+  }
+});
+
+//to delete specific thread
+router.delete("/thread/:threadId", async (req, res) => {
+  const { threadId } = req.params;
+  try {
+    const deletedThread = await Thread.findOneAndDelete({ threadId });
+
+    if (!deletedThread) {
+      res.status(404).json({ error: "Thread not found" });
+    }
+    res.status(200).json({ success: "Thread was deleted" });
+
+    console.log(deletedThread);
+    // res.json(deletedThread);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: "failed to delete this thread" });
   }
 });
 
