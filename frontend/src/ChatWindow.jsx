f34806d583810d36260f2a308af918a143af1d6f
darxh <darshanyavagal@gmail.com>
2025-10-20 14:27:18 +0530

    building chatwindow

--- frontend/src/ChatWindow.jsx
+++ frontend/src/ChatWindow.jsx
@@ -1,9 +1,30 @@
+import Chat from "./Chat";
 import "./Chatwindow.css";
 
 function Chatwindow() {
-  return <div className="main">
-    <p>Chatwindow</p>
-  </div>;
+  return (
+    <div className="chatWindow">
+      <div className="navbar">
+        <span>
+          Cognito<i class="fa-solid fa-angle-down"></i>
+          <div className="userIcon">
+            <span>
+              <i class="fa-solid fa-user"></i>
+            </span>
+          </div>
+        </span>
+      </div>
+
+      <Chat />
+      <div className="chatInput">
+        <div className="userInput">
+          <input type="text" placeholder="Ask anything"/>
+          <div id="submit"><i class="fa-solid fa-paper-plane"></i></div>
+        </div>
+        <p className="info">Gognito can make mistakes, so double-check it</p>
+      </div>
+    </div>
+  );
 }
 
 export default Chatwindow;
