6c0dc0adb672c9f9d0fcb1b60707dc9ad7265db1
darxh <darshanyavagal@gmail.com>
2025-10-23 16:08:33 +0530

    fixing style

--- frontend/src/ChatWindow.jsx
+++ frontend/src/ChatWindow.jsx
@@ -7,19 +7,22 @@ function Chatwindow() {
       <div className="navbar">
         <span>
           Cognito<i class="fa-solid fa-angle-down"></i>
-          <div className="userIcon">
-            <span>
-              <i class="fa-solid fa-user"></i>
-            </span>
-          </div>
         </span>
+        <div className="userIcon">
+          <span>
+            <i class="fa-solid fa-user"></i>
+          </span>
+        </div>
       </div>
 
       <Chat />
+
       <div className="chatInput">
         <div className="userInput">
-          <input type="text" placeholder="Ask anything"/>
-          <div id="submit"><i class="fa-solid fa-paper-plane"></i></div>
+          <input type="text" placeholder="Ask anything" />
+          <div id="submit">
+            <i class="fa-solid fa-paper-plane"></i>
+          </div>
         </div>
         <p className="info">Gognito can make mistakes, so double-check it</p>
       </div>
--- frontend/src/Sidebar.jsx
+++ frontend/src/Sidebar.jsx
@@ -1,12 +1,9 @@
-import Chatwindow from "./ChatWindow";
 import "./Sidebar.css";
 
 function Sidebar() {
   return (
     <div>
       <section className="sidebar">
-        
-        {/* new chat button */}
         <button>
           <img src="src/assets/blacklogo.png" className="logo" alt="logo" />
           <span>
@@ -14,14 +11,12 @@ function Sidebar() {
           </span>
         </button>
 
-        {/* history */}
         <ul className="history">
           <li>history1</li>
           <li>history2</li>
           <li>history3</li>
         </ul>
 
-        {/* sign */}
         <div className="sign">
           <p>By drash &hearts;</p>
         </div>
