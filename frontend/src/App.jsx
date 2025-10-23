4d39fdbd5da51712b3cfc8c5e0c9591f894b890f
darxh <darshanyavagal@gmail.com>
2025-10-23 18:24:50 +0530

    functionality-get reply for promts

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,9 +3,23 @@ import Chat from "./Chat";
 import Chatwindow from "./ChatWindow";
 import Sidebar from "./Sidebar";
 import { MyContext } from "./MyContext";
+import { useState } from "react";
+import { v1 as uuid } from "uuid";
 
 function App() {
-  const providerValues = {}; //pasiing values
+  const [prompt, setPrompt] = useState("");
+  const [reply, setReply] = useState(null);
+  const [currThreadId, setCurrThreadId] = useState(uuid());
+
+  const providerValues = {
+    prompt,
+    setPrompt,
+    reply,
+    setReply,
+    currThreadId,
+    setCurrThreadId,
+  }; //pasiing values
+
   return (
     <div className="app">
       <MyContext.Provider value={providerValues}>
--- frontend/src/ChatWindow.jsx
+++ frontend/src/ChatWindow.jsx
@@ -1,16 +1,44 @@
+import { useContext } from "react";
 import Chat from "./Chat";
 import "./Chatwindow.css";
+import { MyContext } from "./MyContext";
 
 function Chatwindow() {
+  const { prompt, setPrompt, reply, setReply, currThreadId, setCurrThreadId } =
+    useContext(MyContext);
+
+  const getReply = async () => {
+    console.log("message", prompt, "threadId", currThreadId);
+    const options = {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify({
+        message: prompt,
+        threadId: currThreadId,
+      }),
+    };
+
+    try {
+      const response = await fetch("http://localhost:8080/api/chat", options);
+      const result = await response.json();
+      console.log("recived resilt:", result);
+      setReply(result.reply);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
     <div className="chatWindow">
       <div className="navbar">
         <span>
-          Cognito<i class="fa-solid fa-angle-down"></i>
+          Cognito<i className="fa-solid fa-angle-down"></i>
         </span>
         <div className="userIconDiv">
           <span className="userIcon">
-            <i class="fa-solid fa-user"></i>
+            <i className="fa-solid fa-user"></i>
           </span>
         </div>
       </div>
@@ -19,9 +47,17 @@ function Chatwindow() {
 
       <div className="chatInput">
         <div className="inputBox">
-          <input type="text" placeholder="Ask anything" />
-          <div id="submit">
-            <i class="fa-solid fa-paper-plane"></i>
+          <input
+            type="text"
+            placeholder="Ask anything"
+            value={prompt}
+            onChange={(e) => setPrompt(e.target.value)}
+            onKeyDown={(e) => {
+              e.key === "Enter" ? getReply() : "";
+            }}
+          />
+          <div id="submit" onClick={getReply}>
+            <i className="fa-solid fa-paper-plane"></i>
           </div>
         </div>
         <p className="info">Gognito can make mistakes, so double-check it</p>
